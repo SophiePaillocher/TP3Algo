b7183b6d779b332979046d828a9f4ab2d1b588a3
SophiePaillocher <sophie.paillocher@hotmail.fr>
2018-11-28 09:02:06 +0100

    Initial commit

---
+++ src/Graph.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+classe de graphe non orientés permettant de manipuler en même temps des arcs (orientés) pour pouvoir stocker un arbre couvrant, en plus du graphe
+*/
+
+public class Graph implements Iterable<Edge>{
+
+	int order;
+	int edgeCardinality;
+	ArrayList<LinkedList<Edge>> adjacency;
+	ArrayList<LinkedList<Arc>> inAdjacency;
+	ArrayList<LinkedList<Arc>> outAdjacency;
+	
+	public boolean isVertex(int index) {
+	    return (index>=0 & index < order);
+	}
+
+	/**
+	 * Retourne un tableau vide
+	 * @param size le nombre de lignes du tableau
+	 * @param <T> le type des éléments contenus dans le tableau
+	 * @return Le tableau
+	 */
+	public <T> ArrayList<LinkedList<T>> makeList(int size) {
+		ArrayList<LinkedList<T>> res = new ArrayList<>(size);
+		for(int i = 0; i <= size; i++) {
+			res.add(null);			
+		}
+		return res;
+	}
+	
+	public Graph(int upperBound) {
+	    order = 0;
+	    adjacency = makeList(upperBound);
+	}
+	
+	public void addVertex(int indexVertex) {
+	    // à remplir
+	}
+	
+	public void ensureVertex(int indexVertex) {
+	    // à remplir
+	}	
+	
+	public void addArc(Arc arc) {
+	    // à remplir
+	}
+	
+	public void addEdge(Edge e) {
+	    // à remplir
+	}
+	
+}
