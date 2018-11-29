510dc360c241183fb20f5630fce0d673ddfa76f6
SophiePaillocher <43413085+SophiePaillocher@users.noreply.github.com>
2018-11-29 11:16:15 +0100

    Update Vertex.java
    Ajout des méthodes getName et getId

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,9 +1,17 @@
 public class Vertex{
 
-  String name;
-  int id;
+  private String name;
+  private int id;
   
   public Vertex(String name, int id){
     this.name = name;
     this.id = id;
   }
+  
+public String getName(){
+  return name;
+}
+  
+public int getId (){
+  return id;
+}
